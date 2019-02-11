23cd72402ef56971912b4f3d531cc42a8df86045
kanish <kanish@qburst.com>
2019-02-11 10:50:55 +0530

    changes

--- backend/start.sh
+++ backend/start.sh
@@ -5,4 +5,4 @@ echo Starting Gunicorn.
 sleep 5
 DOCKERRUN=1 python manage.py makemigrations
 DOCKERRUN=1 python manage.py migrate
-exec gunicorn proj_settings.wsgi:application --bind 0.0.0.0:8000 --workers 3 --access-logfile logfile
+exec gunicorn proj_settings.wsgi:application --bind 0.0.0.0:9000 --workers 3 --access-logfile logfile
